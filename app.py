f60168a589f799235ea0b7e8357f6582fb22e24f
Anthony Salemo <ajssalemo@gmail.com>
2022-03-02 20:07:39 -0500

    initial commit

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/")
 def helloWorld():
-  return "Deployed with a custom command (3)"
+  return "Deployed with a Azure DevOps"
