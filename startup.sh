79900653af22174360448fc3bf899d14d59fec02
Anthony Salemo <ajssalemo@gmail.com>
2022-11-23 17:09:55 -0500

    initial commit

---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/.venv/lib/site-packages"
+GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' --chdir=/home/site/wwwroot" gunicorn app:app
