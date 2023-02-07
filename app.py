3e4ecb12dd4afc25b6d3ca662ec7d8e8232e8088
Anthony Salemo <ajssalemo@gmail.com>
2023-02-07 14:42:14 -0500

    testing with no virtual env

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/")
 def helloWorld():
-  return "Deployed with Github Actions!"
+  o = os.environ.get('TEST')
+  return f"Deployed with Github Actions! {o}"
