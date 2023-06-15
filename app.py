736a829dea6eb080f964aac90926bd7ffaf4178e
Anthony Salemo <ajssalemo@gmail.com>
2023-06-15 17:31:04 -0400

    testing with slots

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 from flask import Flask
-<<<<<<< HEAD
-=======
-from dotenv import load_dotenv
-
-import os
-
-load_dotenv()
->>>>>>> 16d4a8dd562c7191e7222b24007dc6578c95f88f
 
 app = Flask(__name__)
 
 @app.route("/")
-<<<<<<< HEAD
 def index():
   return "azure-webaps-linux-python-flask-basic"
 
@@ -20,8 +11,3 @@ def index():
 def health():
   print("Health Check endpoint hit")
   return "OK";
-=======
-def helloWorld():
-  o = os.environ.get('TEST')
-  return f"Deployed with Github Actions! - {o}"
->>>>>>> 16d4a8dd562c7191e7222b24007dc6578c95f88f
