d6480a39055ef310097e95b4588ba4fcf4adc066
Anthony Salemo <ajssalemo@gmail.com>
2024-03-12 08:37:02 -0400

    initial commit

--- init_container.sh
+++ init_container.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-gunicorn -b 0.0.0.0 app:app --timeout 600 & \
-    gunicorn app:app -w 3 --certfile /app/cert.pem --keyfile /app/key.pem -b 0.0.0.0:8443
+# Get env vars in the Dockerfile to show up in the SSH session
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
+gunicorn -b 0.0.0.0:8000 app:app --timeout 600 --access-logfile '-' --error-logfile '-'
--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/.venv/lib/site-packages"
-GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' --chdir=/home/site/wwwroot" gunicorn app:app
+echo "startup.sh hit"
+# export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/.venv/lib/site-packages"
+# GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' --chdir=/home/site/wwwroot" gunicorn app:app
