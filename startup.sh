dd8848318ad29931d156ea28aa29e086c23465ee
Anthony Salemo <ajssalemo@gmail.com>
2024-03-13 11:45:50 -0400

    delete startup.sh

--- startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "startup.sh hit"
-# export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/.venv/lib/site-packages"
-# GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' --chdir=/home/site/wwwroot" gunicorn app:app
